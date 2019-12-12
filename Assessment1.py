bfc80b1802b9f8a5693170d9ff820eb8595775df
gy19yh <56163822+gy19yh@users.noreply.github.com>
2019-12-12 21:51:23 +0000

    Add files via upload

--- Assessment1.py
+++ Assessment1.py
@@ -15,44 +15,50 @@ import matplotlib
 import agentframework
 matplotlib.use('TkAgg')
 
-num_of_agents = 10
-num_of_iterations = 100
-agents = []
 
-fig = matplotlib.pyplot.figure(figsize=(7, 7))
+###### Set the variables ########
+num_of_agents = 10  
+num_of_iterations = 100 
+agents = [] # A list named 'agents'
+
+fig = matplotlib.pyplot.figure(figsize=(7, 7)) # set the size of the windows
 ax = fig.add_axes([0, 0, 1, 1])
 
-environment = []
-with open('in.txt', newline='') as csvfile: # open the data file
+
+######### Read the data #########
+environment = [] # A list name 'environment'
+with open('in.txt', newline='') as csvfile: # open the data file named in.txt
     reader = csv.reader(csvfile, delimiter=',') # read the data and seperate the data with comma
     for row in reader: # A list of rows
-       
-        rowlist = []# A list of rows
-        for value in row: # A list of value
-            rowlist.append(int(value))
-        environment.append(rowlist)
+        rowlist = []
+        for value in row: 
+            rowlist.append(int(value)) # add the data value into rowlist
+        environment.append(rowlist) # add the 'rowlist' into 'environmrnt'
+
             
-# Make the agents.
-for i in range(num_of_agents):
-    agents.append(agentframework.Agent(environment,agents,20))
+######### Make the agents #######
+for i in range(num_of_agents): # add 10 elements into the list
+    agents.append(agentframework.Agent(environment,agents,20)) 
 
 carry_on = True	
-	
-def update(frame_number):
+
+
+##### Defined the functions #####	
+def update(frame_number): # update the images for each 'sheeps' move
     
-    fig.clear()   
-    global carry_on
+    fig.clear()   # clear the value in fig
+    global carry_on # call the global variable
     
-    for i in range(num_of_agents):        
-        agents[i].move()
+    for i in range(num_of_agents):  # call the functions that we create in the agnetframework aim to update the value of each agents      
+        agents[i].move() 
         agents[i].eat()       
-        agents[i].share_with_neighbours()
+        agents[i].share_with_neighbourhoods()
 
     if random.random() < 0.1:
         carry_on = False
         print("stopping condition")
     
-    for i in range(num_of_agents):
+    for i in range(num_of_agents): # assign the value to the scatter plots
         matplotlib.pyplot.scatter(agents[i].x,agents[i].y)
         #print(agents[i][0],agents[i][1])
     matplotlib.pyplot.imshow(environment)
@@ -63,16 +69,21 @@ def gen_function(b = [0]):
     while (a < 10) & (carry_on) :
         yield a			# Returns control and waits next call.
         a = a + 1
-
-def run():
+        
+# define the function aims to run the animation
+def run(): 
     animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)
     canvas.draw()
+    
 
+######## Create the Windows #########  
 root = tkinter.Tk() # Main window.
-root.wm_title("Model") # named the title of the windouws
+root.wm_title("Model") 
 canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(fig, master=root)
 canvas._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
 
+
+######## Create 'Run' botton #########
 menu_bar = tkinter.Menu(root)
 root.config(menu=menu_bar)
 model_menu = tkinter.Menu(menu_bar)
--- agentframework.py
+++ agentframework.py
@@ -8,6 +8,7 @@ Created on Fri Nov  1 13:40:22 2019
 import random
 
 class Agent():
+    # initilize the agent class
     def __init__ (self,environment,agents,neighbourhood):
         self.y = random.randint(0,299)
         self.x = random.randint(0,299)
@@ -15,7 +16,8 @@ class Agent():
         self.agents = agents
         self.neighbourhood = neighbourhood
         self.store = 0
-    
+  
+    # define a function to make the agents move twice in a 300 * 300 frame
     def move(self):
         if random.random() < 0.5:
             self.y = (self.y + 1) % 300
@@ -26,7 +28,8 @@ class Agent():
             self.x = (self.x + 1) % 300
         else:
             self.x = (self.x - 1) % 300
-                       
+    
+    # defined a function to make the agents 'eat' the 'environment'                   
     def eat(self):
         if self.environment[self.y][self.x] > 10:
             self.environment[self.y][self.x] -= 10
@@ -41,10 +44,11 @@ class Agent():
                 self.store = ave
                 agent.store = ave
 
+    # defined a function for calculating the distance between two agents
     def distance_between(self, agent):
         return (((self.x - agent.x)**2)+((self.y - agent.y)**2))**0.05
 
-    def share_with_neighbours(self):
+    def share_with_neighbourhoods(self):
         for agent in self.agents:
             dist = self.distance_between(agent)
             if dist <= self.neighbourhood:
